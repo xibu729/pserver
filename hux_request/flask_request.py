f15541715d06944961a8c9d280098d620581c7ee
xibu729 <43806652@qq.com>
2019-02-12 10:23:07 +0800

    modify

---
+++ hux_request/flask_request.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return '<h1>hello world</h1>'
+
+
+@app.route('/user/<name>')
+def get_username(name):
+    return '<h1>hello %s</h1>' % name
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
